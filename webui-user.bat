7edce4b84e2098da30dfeb1ce3d694617fed917b
koisonn manes mana <104087468+Amemorte99@users.noreply.github.com>
2025-02-18 13:36:56 +0000

    hy

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS= --xformers --autolaunch --theme dark
+set COMMANDLINE_ARGS= --xformers --autolaunch --theme dark --skip-torch-cuda-test
 
 call webui.bat
